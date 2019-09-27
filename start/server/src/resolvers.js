aa7af2400f7caefe1c3d52116b0f8d143c0a70a9
Mari Queiroz <mqueiroz@thoughtworks.com>
2019-09-27 11:33:01 -0300

    terceiro encontro

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,4 +1,4 @@
-const {paginateResults} = require('./utils');
+const { paginateResults } = require('./utils');
 
 module.exports = {
     Query: {
@@ -17,19 +17,74 @@ module.exports = {
                 cursor: launches.length ? launches[launches.length - 1].cursor : null,
                 hasMore: launches.length
                     ? launches[launches.length - 1].cursor !==
-                      allLaunches[allLaunches.length - 1].cursor
+                    allLaunches[allLaunches.length - 1].cursor
                     : false
             }
         },
-        launch: (_, {id}, {dataSources}) =>
-            dataSources.launchAPI.getLaunchById({launchId: id}),
-        me: (_, __, {dataSources}) => dataSources.userAPI.findOrCreateUser()
+        launch: (_, { id }, { dataSources }) =>
+            dataSources.launchAPI.getLaunchById({ launchId: id }),
+        me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+    },
+    Mutation: {
+        bookTrips: async (_, { launchIds }, { dataSources }) => {
+            const results = await dataSources.userAPI.bookTrips({ launchIds });
+            const launches = await dataSources.launchAPI.getLaunchesByIds({ launchIds });
+
+            return {
+                success: results && results.length === launchIds.length,
+                message: results.length === launchIds.length
+                    ? 'trips booked successfully'
+                    : `the following laucnhes couldn't be booked: ${launchIds.filter(id => !results.includes(id))}`,
+                launches,
+            };
+        },
+        cancelTrip: async (_, { launchId }, { dataSources }) => {
+            const result = await dataSources.userAPI.cancelTrip({ launchId });
+
+            if (!result)
+                return {
+                    success: false,
+                    message: 'failed to cancel trip',
+                };
+
+            const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+            return {
+                success: true,
+                message: 'trip cancelled',
+                launches: [launch],
+            };
+        },
+        login: async (_, { email }, { dataSources }) => {
+            const user = await dataSources.userAPI.findOrCreateUser({ email });
+            if (user) {
+                return Buffer.from(email).toString('base64');
+            }
+        }
     },
     Mission: {
         missionPatch: (mission, { size } = { size: 'LARGE' }) => {
             return size === 'SMALL'
                 ? mission.missionPatchSmall
                 : mission.missionPatchLarge;
+        },
+    },
+    Launch: {
+        isBooked: async (launch, _, { dataSources }) =>
+            dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+    },
+    User: {
+        trips: async (_, __, { dataSources }) => {
+            // get ids of launches by user
+            const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+
+            if (!launchIds.length) return [];
+
+            // look up those launches by their ids
+            return (
+                dataSources.launchAPI.getLaunchesByIds({
+                    launchIds,
+                }) || []
+            );
         },
     },
 };
