eaa72e5bf578be0b84020e4f21a0dadabd4629a0
Mari Queiroz <mqueiroz@thoughtworks.com>
2019-10-04 11:30:34 -0300

    finalizando parte 6 do tutorial

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -20,12 +20,15 @@ const server = new ApolloServer({
     context: async ({ req }) => {
         const auth = (req.headers && req.headers.authorization) || '';
         const email = Buffer.from(auth, 'base64').toString('ascii');
-        if(!isEmail.validate(email)) return { user: null }
+        if (!isEmail.validate(email)) return { user: null }
 
         const users = await store.users.findOrCreate({ where: { email } });
         const user = (users && users[0]) || null;
 
         return { user: { ...user.dataValues } };
+    },
+    engine: { 
+        apiKey: process.env.ENGINE_API_KEY,
     },
 });
 
